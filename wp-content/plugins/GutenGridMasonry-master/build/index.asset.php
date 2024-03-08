bdbfc9e75035e22eb7da0615a72d42fe24a1b1e8
Maitri Prajapti <maitri@zealousweb.com>
2024-03-08 13:26:46 +0530

    adding script

--- wp-content/plugins/GutenGridMasonry-master/build/index.asset.php
+++ wp-content/plugins/GutenGridMasonry-master/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'ab36912b738553354c1f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '8619f14eea6a196b383e');
